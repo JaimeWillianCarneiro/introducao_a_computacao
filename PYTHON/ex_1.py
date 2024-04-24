6dc0f12b49be3aafafa48233df84e43eaa0d3b75
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-04-24 08:34:24 -0300

    Ex 1 pyhton

---
+++ PYTHON/ex_1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+media = (num1 +num2)/2
+
+print("Media:",  media)
---
+++ PYTHON/ex_2.py
@@ -0,0 +1,3 @@
+tempCelsius = float(input("Digite a temperatura (em ºC): "))
+tempF = tempCelsius*1.8 + 32
+print("Temperatura convertida para Fahrenheit:", tempF)
