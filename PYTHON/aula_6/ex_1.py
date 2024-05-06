35a68ad98f8111871478536a4cc9540ab03d7fe0
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-05-06 09:10:27 -0300

    finish

---
+++ PYTHON/aula_6/ex_1.py
@@ -0,0 +1,23 @@
+def calcular_total_compras(valor: float):
+    
+
+    if  500 <= valor <1000:
+         valor = valor*0.9
+    elif 1000 <= valor < 1500:
+         valor = valor*0.8
+    elif 1500 <= valor:
+         valor = 0.3*valor
+    
+
+    print(f"Valor a ser pago: R$ {valor}")
+
+
+
+while True:
+     valor = float(input("Digite o valor total das compras: (ou digite '0' para sair) "))
+     if valor==0:
+          print("Programa finalizado!")
+          break
+          
+     else:
+          calcular_total_compras(float(valor))
