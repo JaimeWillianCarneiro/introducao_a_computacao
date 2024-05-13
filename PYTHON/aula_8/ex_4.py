b19b9fe0f7b8ae4ecb4d70b8e696957579a94ee7
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-05-13 09:07:27 -0300

    aa

--- PYTHON/aula_8/ex_4.py
+++ PYTHON/aula_8/ex_4.py
@@ -2,3 +2,10 @@
 Inverter lista
 """
 def inverter_lista(lista: list):
+    if len(lista)==1:
+        return lista
+    return [lista[-1]] + inverter_lista(lista[:-1])
+
+lista = [1, 2, 3,4,5]
+print(inverter_lista(lista))
+# 5, 4, 3, 2, 1
