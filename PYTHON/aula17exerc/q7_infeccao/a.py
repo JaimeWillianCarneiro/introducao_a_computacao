2d468e1626a93188430f7ceafd0b403892502088
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-06-15 01:10:51 -0300

    comentando pra ver o que sai

--- PYTHON/aula17exerc/q7_infeccao/a.py
+++ PYTHON/aula17exerc/q7_infeccao/a.py
@@ -12,21 +12,49 @@ def distancia(pontoA, pontoB):
 
 
 def propagar_infeccao(linhas, colunas, polos)-> list:
-    matriz = [[0]*colunas]*linhas
+    matriz = [[0 for _ in range(colunas)] for _ in range(linhas)]
     
     for polo in polos:
-        coordX = polo[0]
-        coordY = polo[1]
+        coordX = polo[1]
+        coordY = polo[0]
         raio = polo[2]
         
         matriz[coordY][coordX] = 1
         
+        linhas_acima =0
+        #  Vendo quantas linhas acimas posso passar
+        if coordY - raio <0:
+            linhas_acima = coordY
+        else:
+            linhas_acima = raio
+        
+        #  vendo colunas a esquerda posso infectar
+        if coordX - raio <0:
+            colunas_esquerda=coordX
+        else: colunas_esquerda= raio
+        
+        #  Vendo quantas linhas abaixo posso infectar
+        if coordY+ raio > n-1:
+            linhas_abaixo = n-1 - coordY
+        else:
+            linhas_abaixo= raio
+        
+        #  Vendo quantas colunas a direiat posso infectar
+        if coordX + raio > n-1:
+            colunas_direita = n-1 - coordX
+        else:
+            colunas_direita = raio
+        
+            
+        linhas_abaixo =0
+        colunas_direita = 0 
+        colunas_esquerda = 0 
         
         # Analisar a linha atual e as r linhas acima e r linhas 
         #  Abaixo. Sendo assim iriamos analisar (2r+1)*(2r+1)-1 casas
         
         #  Analisando a direita
-        
+    
     pass
 resultado = propagar_infeccao(m, n, polos_de_infeccao)
 print(resultado)
