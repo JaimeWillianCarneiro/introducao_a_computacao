afa35657c79d08338c43115902fb02347a073780
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-06-15 00:53:41 -0300

    tarar

---
+++ simulado_a2/q4.py
@@ -0,0 +1,20 @@
+def v2m(v, num_lin, num_col):
+    #Inicializa a matriz M como uma lista vazia
+    M = []
+    for j in range(num_lin):
+        #Inicializa uma linha vazia
+        linha = []
+        for i in range(num_col):
+            #Adiciona o elemento correto da lista v à linha
+            linha.append(__________)
+        #Adiciona a linha completa à matriz M
+        M.append(__________)
+    return M
+
+
+"""  
+1.d
+2. B
+3. 
+
+"""
